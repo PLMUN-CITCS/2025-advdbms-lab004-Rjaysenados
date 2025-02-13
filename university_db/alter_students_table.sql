f0666fe38dace131af6af36a57aff9010633c578
rjaysenados <senadosr-jay_bsit@plmun.edu.ph>
2025-02-13 08:51:44 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/alter_students_table.sql
+++ university_db/alter_students_table.sql
@@ -1,7 +1,2 @@
 ALTER TABLE `Students`
-`StudentID` INT AUTO_INCREMENT PRIMARY KEY,
-   `FirstName` VARCHAR(50) NOT NULL,
-   `LastName` VARCHAR(50) NOT NULL,
-   `EnrollmentDate` DATE
    `Email` VARCHAR(100);
-);
