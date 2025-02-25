ba317d97f4845bf6867e2b124ce006c12a5a2309
rjaysenados <senadosr-jay_bsit@plmun.edu.ph>
2025-02-26 01:21:51 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 02

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -1,6 +1,10 @@
 CREATE TABLE `Students` (
-   `StudentID` INT AUTO_INCREMENT PRIMARY KEY,
+   `StudentID` INT AUTO INCREMENT PRIMARY KEY,  -- MySQL Example: AUTO_INCREMENT
+   -- For other databases (e.g., PostgreSQL, SQL Server) you might use SERIAL or IDENTITY
    `FirstName` VARCHAR(50) NOT NULL,
    `LastName` VARCHAR(50) NOT NULL,
    `EnrollmentDate` DATE
 );
+
+ALTER TABLE `Students`
+ADD `Email` VARCHAR(100);
